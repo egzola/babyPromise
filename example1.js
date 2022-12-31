9dd427848c935b4b884c0c85d7a3bad7eff9b7cb
Eduardo Zola <egzola@gmail.com>
2022-12-31 18:12:49 -0300

    Add files via upload

--- example1.js
+++ example1.js
@@ -3,7 +3,7 @@
 // Email me at: egzola@gmail.com
 // License: GNU General Public License v3.0 - Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.
 
-
+// Promise will be auto-executed as soon as created
 
 'use strict';
 
